5626263591142cd667b3d21b78d7efa683c3191e
Daniel Rondon <rondondaniel@gmail.com>
2022-08-22 11:10:05 +0200

    Working on upload pause/resume/cancel

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,10 +24,11 @@
           multiple
           counter
           label="Drop Files Here"
+          @change="onFileInput"
         ></v-file-input>
       </v-col>
     </v-row>
-    <v-row class="justify-end">
+    <v-row class="justify-center pa-14">
       <v-btn
         color="primary"
         rounded
@@ -39,12 +40,71 @@
       <v-btn
         color="primary"
         rounded
-        @click="readFileNames"
+        @click="testReactivity"
       >
-        File Names
+        Test
       </v-btn>
     </v-row>
-  
+    <template>
+      <v-data-table
+        dense
+        :headers="headers"
+        :items="progressInfo"
+        :items-per-page="5"
+        hide-default-header
+        item-key="name"
+        class="elevation-1"
+      >
+        <template v-slot:item="{ item }">
+         <v-row>
+          <v-col cols="6" align-self="start">
+              {{ item.name }}
+          </v-col>
+          <v-col cols="3" align-self="center">
+            <v-progress-linear
+            :background-opacity="0.3"
+            :buffer-value="100"
+            :height="25"
+            :width="150"
+            :rounded="true"
+            :value="item.percentage"
+          >
+            <strong>{{ item.percentage }}%</strong>
+          </v-progress-linear>
+          </v-col>
+          <v-col cols="1" align-self="center">
+              <v-btn 
+                class="mx-2"
+                fab
+                dark
+                small
+                color="primary"
+                @click="pause(item.id)"
+              >
+                <v-icon dark>
+                  mdi-pause
+                </v-icon>
+              </v-btn>
+          </v-col>
+          <v-col cols="1" align-self="center">
+              <v-btn 
+                class="mx-2"
+                fab
+                dark
+                small
+                color="error"
+                @click="cancel(item.id)"
+              >
+                <v-icon dark>
+                  mdi-close
+                </v-icon>
+              </v-btn>
+          </v-col>
+          </v-row>
+        </template>
+      
+      </v-data-table>
+    </template>
   </v-container>
 </template>
 
@@ -56,36 +116,91 @@
     data: () => ({
       files: [],
       projectName: null,
-      filesNames: [],
+      progressInfo: [],
+      fileState: [],
+      itemsPerPage: 4,
+      headers: [
+        {
+          text: 'Files',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { 
+          text: 'Percentage (%)', 
+          value: 'percentage' 
+        },
+      ],
     }),
     methods: {
-      readFileNames () {
-        for (let i = 0; i < this.files.length; i++) {          
-          this.filesNames[i] = this.files[i].name
-          console.log(this.files[i].type)
+      onFileInput (_files) {
+        this.progressInfo = [];
+        this.fileState = [];
+
+        for (let i = 0; i < _files.length; i++) {     
+          let _value =  { id: i, name: _files[i].name, percentage: 0 }
+          this.progressInfo.push(_value)
         }
-        console.log(this.filesNames)
+        console.log(this.progressInfo);
+      },
+      testReactivity () {
+        // want to change 3th file percentage to 50
+        let i = 2
+        this.progressInfo.splice(i, 1, { id: i, name: this.files[i].name, percentage: 50 })
+        console.log(this.progressInfo)
       },
       uploadFiles: async function () {
-        for (let i = 0; i < this.files.length; i++) {          
-          const upload = await Storage.put(this.filesNames[i], this.files[i], {
+        for (let i = 0; i < this.files.length; i++) {
+          let value =  { id: i, pause: 0, cancel: 0 }
+          this.fileState.push(value)
+
+          const upload = await Storage.put(this.projectName + "/" + this.files[i].name, this.files[i], {
             level: "private",
             contentType: this.files[i].type,
-               resumable: true,
-               completeCallback: (event) => {
-                   console.log(`${this.filesNames[i]} Successfully uploaded ${event.key}`);
-               },
-               progressCallback: (progress) => {
-                   console.log(`${this.filesNames[i]} Uploaded: ${progress.loaded}/${progress.total}`);
-               },
-               errorCallback: (err) => {
-                   console.error(`${this.filesNames[i]}: Unexpected error while uploading`, err);
-               }
+            resumable: true,
+            completeCallback: (event) => {
+              console.log(`${this.files[i].name} Successfully uploaded ${event.key}`);
+            },
+            progressCallback: (progress) => {
+              this.progressInfo.splice(i, 1, { 
+                id: i,
+                name: this.files[i].name,
+                percentage: Math.ceil(progress.loaded / progress.total * 100) 
+              }),
+              console.log(`${this.files[i].name} Uploaded: ${progress.loaded}/${progress.total}`);
+            },
+            errorCallback: (err) => {
+              console.error(`${this.files[i].name}: Unexpected error while uploading`, err);
+            }
           });
-          console.log(upload)
+          console.log(upload);
+          console.log(this.fileState);
+          // Check user interaction
+          if (this.fileState[i].pause == 1) {
+            upload.pause;
+            console.log("Pause file:", this.files[i].name);
+          } else if (this.fileState[i].cancel == 1) {
+            Storage.cancel(upload);
+            console.log("Pause file:", this.files[i].name);
+          }
         }
-        
-      }
+      },
+      pause (index) {
+        console.log("Pause file:", index);
+        this.fileState.splice(index, 1, { 
+          id: index,
+          pause: 1,
+          cancel: 0,
+        })
+      },
+      cancel (index) {
+        console.log("Cancel file:", index);
+         this.fileState.splice(index, 1, { 
+          id: index,
+          pause: 0,
+          cancel: 1,
+        })
+      },
     }
   }
 </script>
